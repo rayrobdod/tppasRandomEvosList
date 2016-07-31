5afd029660934d3665c7686b29e50479bf30c986
Raymond Dodge <git@rayrobdod.name>
2016-07-31 01:39:06 -0400

    Render of dfb1a73198d8d4233b13f4eb96239030395d5d66

---
+++ style/tableSort.js
@@ -0,0 +1,43 @@
+var tables = document.getElementsByTagName("table");
+
+const SORT_INDICATOR = "↕";
+
+function toArray(x) {
+	var retVal = new Array()
+	for (i = 0; i < x.length; i++) {
+		retVal.push(x[i])
+	}
+	return retVal
+}
+
+function sortTable(tableToSort, columnNo) {
+	var initial = tableToSort.tBodies[0].rows;
+	function compareRows(a,b) {
+		if (a.cells[columnNo].dataset["sort"] < b.cells[columnNo].dataset["sort"]) { return 1; }
+		if (a.cells[columnNo].dataset["sort"] > b.cells[columnNo].dataset["sort"]) { return -1; }
+		else { return 0; }
+	}
+	var sortedArray = toArray(initial).sort(compareRows)
+	
+	var newTableBody = document.createElement("tbody");
+	for(var i = 0; i < sortedArray.length; i++) {
+		newTableBody.appendChild(sortedArray[i].cloneNode(true));
+	}
+	tableToSort.replaceChild(newTableBody, tableToSort.tBodies[0]);
+}
+
+toArray(tables).filter(function(tab) {return tab.classList.contains("pokemon-list")}).forEach(function(tab) {
+		var headerCells = tab.tHead.rows[0].cells
+		for (j = 0; j < headerCells.length; j++) {
+			if (j != 2 && j != 3) {
+				function doThing(cellIndex) {
+					var c = headerCells[cellIndex];
+					c.appendChild(document.createTextNode(SORT_INDICATOR));
+					c.addEventListener("click", function() {
+						sortTable(tab, cellIndex)
+					});
+				}
+				doThing(j)
+			}
+		}
+	})
