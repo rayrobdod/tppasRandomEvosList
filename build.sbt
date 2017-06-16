abc6a8c180a0bbaa462f33cba16e276d7b8e92dc
Raymond Dodge <git@rayrobdod.name>
2017-06-15 21:52:37 -0400

    Add more `map{identity}` for performance

--- build.sbt
+++ build.sbt
@@ -11,3 +11,12 @@ pipelineStages := Nil
 enablePlugins(com.typesafe.sbt.web.SbtWeb)
 
 com.rayrobdod.possibleEvolutions.MyBuild.mySettings
+
+
+
+
+(unmanagedSourceDirectories in Compile) += baseDirectory.value / "project"
+libraryDependencies += ("com.rayrobdod" %% "anti-xml" % "0.7-SNAPSHOT-20150909")
+libraryDependencies += ("com.opencsv" % "opencsv" % "3.4")
+enablePlugins(JmhPlugin)
+(unmanagedSources in Compile) := (unmanagedSources in Compile).value.filter{_.getName != "build.scala"}
---
+++ src/main/scala/PageTemplatesBench.scala
@@ -0,0 +1,28 @@
+package com.rayrobdod.possibleEvolutions
+package benchmark
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.State
+
+object PageTemplatesBench {
+	
+	@State(org.openjdk.jmh.annotations.Scope.Thread)
+	class _State {
+		val accessOrder = (1 to 50).map{DexNo(_)}
+
+		val data = {
+			ListOfPokemon.fromFiles(new java.io.File("C:/Users/Raymond/Documents/Programming/HTML-JS/RandomizedEvolutions/src/main/data/"))
+		}
+	}
+}
+
+class PageTemplatesBench {
+	import ListOfPokemonBench._State
+	
+	@Benchmark
+	def perMonPage(state:_State) = {
+		implicit val config = EvosGame.White2
+		
+		state.accessOrder.map{dexno => PageTemplates.perMonPage(dexno, state.data)}
+	}
+}
